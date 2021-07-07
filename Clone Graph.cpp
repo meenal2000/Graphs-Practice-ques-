91d24caba353addf69f3d8b7c2620d1122319d75
meenal2000 <53430411+meenal2000@users.noreply.github.com>
2021-07-07 07:30:00 +0530

    Create Clone Graph.cpp

---
+++ Clone Graph.cpp
@@ -0,0 +1,21 @@
+time - O(n+m)
+space - O(n)
+
+unordered_map<int,Node*> mp; // value and address of new node
+Node* solve(Node* node)
+{
+    if(!node)
+        return NULL;
+    if(mp.find(node->val)!=mp.end())
+        return mp[node->val];
+    Node* tmp = new Node(node->val);
+    mp[tmp->val] = tmp;
+    for(Node* neigh : node->neighbors)
+    {
+        tmp->neighbors.push_back( solve(neigh) );
+    }
+    return tmp;
+}
+Node* cloneGraph(Node* node) {
+    return solve(node);
+}
